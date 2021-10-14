b10feeb4d1b235c15898a6c54c4022a53243420a
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-10-14 09:24:23 +0300

    Add serializer

--- tasks/settings.py
+++ tasks/settings.py
@@ -161,7 +161,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 django_heroku.settings(locals())
-AUTH_USER_MODEL = 'tasks_api.User'
+# AUTH_USER_MODEL = 'tasks_api.User'
 
 
 REST_FRAMEWORK = {
