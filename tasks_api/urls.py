2dd91911c92b14dc34337f782fb468e5c61d379b
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-10-14 09:31:25 +0300

    Update api views

--- tasks_api/urls.py
+++ tasks_api/urls.py
@@ -3,4 +3,6 @@ from .import views as api_views
 
 urlpatterns = [ 
     path('',api_views.apiOverview,name="api-overview"),
+    path('task-list',api_views.taskList,name="task-list"),
+
 ]
--- tasks_api/views.py
+++ tasks_api/views.py
@@ -4,7 +4,18 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from tasks_api.models import Task
+from .serializers import TaskSerializer
+from .models import Task
+
 # Create your views here.
 @api_view(['GET'])
 def apiOverview(request):
     pass
+
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks,many = True)
+    return Response(serializer.data)
